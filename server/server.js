85df83d5595a6cb4d510908987d57eaf2c21ef80
John Doe <johndoe@exemple.com>
2025-01-06 14:29:57 +0100

    Retrieve data successful

--- server/server.js
+++ server/server.js
@@ -4,10 +4,24 @@ const cors = require("cors");
 // Create the application with express
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+// Connect database with server
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "ws",
+});
+
+// API start with nodemon
+app.get("/api/get", (req, res) => {
+  const request = "SELECT * FROM project ORDER BY created_at DESC";
+  db.query(request, (error, result) => {
+    res.send(result);
+  });
+});
+
 // Start on port 8000
 app.listen(8000);
-
-// URL + nodemon server.js
-app.get("/testserver", (req, res) => {
-  res.send("Server start successful !");
-});
