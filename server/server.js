b760676a787ade794ef9efcc6ef9042fb1010fa9
John Doe <johndoe@exemple.com>
2025-01-06 15:11:52 +0100

    Create a project

--- server/server.js
+++ server/server.js
@@ -16,11 +16,31 @@ const db = mysql.createPool({
 });
 
 // API start with nodemon
+// READ all projects
 app.get("/api/get", (req, res) => {
   const request = "SELECT * FROM project ORDER BY created_at DESC";
   db.query(request, (error, result) => {
     res.send(result);
   });
+});
+
+// Create a project
+app.post("/api/post", (req, res) => {
+  const { projectname, projectstart, projectend, projectcomment } = req.body;
+  const request =
+    "INSERT INTO project (projectname, projectstart, projectend, projectcomment) VALUES (?, ?, ?, ?)";
+
+  db.query(
+    request,
+    [projectname, projectstart, projectend, projectcomment],
+    (error, result) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send("Erreur lors de l'ajout du projet."); // Renvoie une erreur en cas d'échec
+      }
+      res.status(200).send("Projet ajouté avec succès."); // Renvoie un succès
+    }
+  );
 });
 
 // Start on port 8000
